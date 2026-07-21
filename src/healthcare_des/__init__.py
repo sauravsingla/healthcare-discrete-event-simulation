aad90c7c060f0ef5759097ae8af933eb6df7fc0d
SAURAV SINGLA <sauravsingla08@gmail.com>
2026-07-21 17:08:39 +0530

    Expose advanced simulation API

--- src/healthcare_des/__init__.py
+++ src/healthcare_des/__init__.py
@@ -1,6 +1,26 @@
 """Healthcare demand and capacity discrete-event simulation."""
 
+from .advanced_model import (
+    AdvancedScenarioConfig,
+    AdvancedSimulationResult,
+    CapacityWindow,
+    MachineWindow,
+    run_advanced_once,
+    run_advanced_replications,
+    summarise_advanced,
+)
 from .model import ScenarioConfig, SimulationResult, run_replications
 
-__all__ = ["ScenarioConfig", "SimulationResult", "run_replications"]
-__version__ = "0.1.0"
+__all__ = [
+    "ScenarioConfig",
+    "SimulationResult",
+    "run_replications",
+    "AdvancedScenarioConfig",
+    "AdvancedSimulationResult",
+    "CapacityWindow",
+    "MachineWindow",
+    "run_advanced_once",
+    "run_advanced_replications",
+    "summarise_advanced",
+]
+__version__ = "0.2.0"
