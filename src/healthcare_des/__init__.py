33c05d0d5f13879c739537c78a10ff775679f4cd
SAURAV SINGLA <sauravsingla08@gmail.com>
2026-07-21 17:12:23 +0530

    Expose research validation API

--- src/healthcare_des/__init__.py
+++ src/healthcare_des/__init__.py
@@ -10,6 +10,14 @@ from .advanced_model import (
     summarise_advanced,
 )
 from .model import ScenarioConfig, SimulationResult, run_replications
+from .research_validation import (
+    calibrate_parameters,
+    confidence_interval,
+    equivalence_report,
+    fit_distributions,
+    fit_hourly_profile,
+    save_distribution_plots,
+)
 
 __all__ = [
     "ScenarioConfig",
@@ -22,5 +30,11 @@ __all__ = [
     "run_advanced_once",
     "run_advanced_replications",
     "summarise_advanced",
+    "fit_hourly_profile",
+    "confidence_interval",
+    "equivalence_report",
+    "fit_distributions",
+    "save_distribution_plots",
+    "calibrate_parameters",
 ]
-__version__ = "0.2.0"
+__version__ = "0.3.0"
